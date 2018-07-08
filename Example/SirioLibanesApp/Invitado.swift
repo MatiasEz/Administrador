b8292c48cd43e24266ab8144c5c73b11be336aa1
Ezcurra <matialeezcurra@gmail.com>
2018-07-07 22:03:11 -0300

    Reception Changes

---
+++ Example/SirioLibanesApp/Invitado.swift
@@ -0,0 +1,13 @@
+//
+//  Invitado.swift
+//  SirioLibanesApp_Example
+//
+//  Created by Federicuelo on 27/6/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class Invitado: NSObject {
+
+}
---
+++ Example/SirioLibanesApp/ReceptionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReceptionTableViewCell.swift
+//  SirioLibanesApp_Example
+//
+//  Created by Federicuelo on 25/6/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ReceptionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
